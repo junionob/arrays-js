d1f26baec7710bc69a109b6fb2feeaf060652ea7
junionob <junio.waldomiro@gmail.com>
2023-06-06 09:53:25 -0300

    1

---
+++ index.js
@@ -0,0 +1,5 @@
+const arrayVazia = [,,,];
+
+arrayVazia.push(50)
+console.log(arrayVazia);
+console.log(arrayVazia.length);
---
+++ media.js
@@ -0,0 +1,4 @@
+const nota = [10, 6.5, 8, 7.5];
+const media = (nota[0] + nota[1] + nota[2] + nota[3]) / nota.length;
+
+console.log(media)
